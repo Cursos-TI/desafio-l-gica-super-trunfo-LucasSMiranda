a7c100b7505dde179148e7ce4f7f99e19b499e8e
Lucas Miranda <104878497+LucasSMiranda@users.noreply.github.com>
2026-04-16 00:23:38 +0000

    Finalizando alterações

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -54,12 +54,31 @@ int main() {
     {
     case 1:
         atributo1_carta1 = populacao1; atributo1_carta2 = populacao2;
+        printf("Você escolheu População.\n");
+        if (populacao1 > populacao2) {
+         printf("Pais1 tem maior população.\n");
+     } else {
+         printf("Pais2 tem maior população.\n");
+     }
         break;
     case 2:
         atributo2_carta1 = area1; atributo2_carta2 = area2;
+        printf("Você escolheu área.\n");
+        if (area1 > area2) {
+        printf("Area1 maior que area2./n");
+    } else{
+        printf("Area2, maior que area1.");
+    }
         break;
     case 3:
         atributo3_carta1 = pib1; atributo3_carta2 = pib2;
+        printf("Você escolheu pib.\n");
+        if (pib1 > pib2){
+        printf("Pib1 > pib2.\n");
+    } else{
+        printf("Pib2 maior quer pib1");
+
+    }     
 
     default:
         printf("Opção errada.\n");
@@ -68,28 +87,24 @@ int main() {
     // Utilize estruturas de decisão como if, if-else para comparar atributos como população, área, PIB, etc.
 
     // Exemplo:
-    if (populacao1 > populacao2) {
-         printf("Pais1 tem maior população.\n");
-     } else {
-         printf("Pais2 tem maior população.\n");
-     }
+    
 
-    if (area1 > area2) {
-        printf("Area1 maior que area2./n");
-    } else{
-        printf("Area2, maior que area1.");
-    }
+    
 
-    if (pib1 > pib2){
-        printf("Pib1 > pib2.\n");
-    } else{
-        printf("Pib2 maior quer pib1");
-
-    }        
+       
 
     
 
     // Exibição dos Resultados:
+
+    if(atributo1_carta1 > atributo1_carta2){
+        printf("Brasil ganhou.\n");
+    }else{
+        printf("Argentina ganhou.\n");
+    }
+
+
+
     // Após realizar as comparações, exiba os resultados para o usuário.
     // Certifique-se de que o sistema mostre claramente qual carta venceu e com base em qual atributo.
 
